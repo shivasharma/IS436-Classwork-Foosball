040478fb20c92e3369867c85a24bc04f9ff86e45
Shiva Sharma <ss12@umbc.edu>
2024-10-09 10:30:24 -0400

    Create init.d

---
+++ init.d
@@ -0,0 +1,7 @@
+  CREATE Table dbo.Customer(
+       CustId INT NOT NULL,
+       CustName VARCHAR(150) ,
+       CustCode VARCHAR(20),
+       CustMailId VARCHAR(30)
+       PRIMARY KEY (custId)
+  )
